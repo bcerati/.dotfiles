9edf1989252c9ec07b6351d449e4cadd29e28e8f
Boris CERATI <contact@boris-cerati.fr>
2023-04-11 18:31:36 +0200

    Configuration for yamlls

--- config/nvim/after/plugin/copilot.lua
+++ config/nvim/after/plugin/copilot.lua
@@ -1,3 +1,5 @@
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-W>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-S-Up>", 'copilot#Next()', { silent = true, expr = true })
+vim.g.copilot_filetypes = { yaml = true }
 
--- config/nvim/lua/bobo_dev/mappings.lua
+++ config/nvim/lua/bobo_dev/mappings.lua
@@ -1,6 +1,8 @@
-vim.keymap.set('n', '<leader>e', '<cmd>Ex<CR>');
-
 vim.keymap.set('n', '<C-d>', '<C-d>zz');
 vim.keymap.set('n', '<C-u>', '<C-u>zz');
 vim.keymap.set('n', 'n', 'nzzzv');
 vim.keymap.set('n', 'N', 'Nzzzv');
+
+-- resize with arrows
+vim.keymap.set('n', '<A-Right>', '<cmd>:vertical resize +5<CR>');
+vim.keymap.set('n', '<A-Left>', '<cmd>:vertical resize -5<CR>');
